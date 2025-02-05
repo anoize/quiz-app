b81f211a10b4c5dc7fa739363d374ecfe5c67867
anoize <152575195+anoize@users.noreply.github.com>
2025-02-05 15:06:52 +0700

    define

---
+++ src/component/quiz.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import QuestionsData from "./data/QuestionData";
+
+const Quiz = ()=>{
+    //console.log(QuestionsData);
+    const [current, setCurrent] = useState(0)
+    return(
+        <div className="quiz">
+
+            <h1>{QuestionsData[current].question}</h1>
+            <div className="choices">
+                    <button>{QuestionsData[current].A}</button>
+                    <button>{QuestionsData[current].B}</button>
+                    <button>{QuestionsData[current].C}</button>
+                    <button>{QuestionsData[current].D}</button>
+            </div>
+
+
+        </div>
+    )
+}
+export default Quiz;
---
+++ src/component/score.js
@@ -0,0 +1,10 @@
+const Score = ()=>{
+    return(
+        <div className="score">
+            <h1>Score Component</h1>
+
+
+        </div>
+    )
+}
+export default Score;
