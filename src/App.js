bd697676a355819db30f3a3cad7b4b6d692a5a5e
anoize <152575195+anoize@users.noreply.github.com>
2025-02-05 15:41:34 +0700

    appfinish

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ export const DataContext = createContext()
 
 function App() {
   const [appState, setAppState] = useState("menu")
+  const [score,setScore] = useState(0)
   return (
-    <DataContext.Provider value={{appState,setAppState}}>
+    <DataContext.Provider value={{appState,setAppState,score,setScore}}>
        <div className="App">
     <h1>Web Development Quiz</h1>
     {appState === "menu" && <Menu/>}
-    {appState === "menu" && <Quiz/>}
-    {appState === "menu" && <Score/>}
+    {appState === "quiz" && <Quiz/>}
+    {appState === "score" && <Score/>}
    </div>
     </DataContext.Provider>
   );
